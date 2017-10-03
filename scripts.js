5d5b83f07aac42b487e1f99301897d65de8ffe66
Damian <damiankub89@gmail.com>
2017-10-03 11:54:24 +0200

    Event

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ var list = document.getElementById('list');
 var add = document.getElementById('addElem');
 
 add.addEventListener('click', function() {
-  var listNumber = document.getElementsByTagName('li').length;
+  var listNumber = list.getElementsByTagName('li').length;
   var element = document.createElement('li');
       element.innerHTML = 'item '+listNumber;
       list.appendChild(element);
